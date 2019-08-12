ddcf75b98a5fc87c3c27c768aafdc2106b39fbe8
Joey <YueZheng@wistronits.com>
2019-08-12 16:15:28 +0800

    Add project files.

---
+++ DotNetFive/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetFive.Models;
+using DotNetFive.ViewModels;
+using Humanizer;
+using Microsoft.Extensions.Options;
+
+namespace DotNetFive.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Content contents = null;
+        public HomeController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        public IActionResult Index()
+        {
+            //return View();
+            //var contents = new List<Content>();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, Title = $"{i}的标题", Contents = $"{i}的内容", Status = 1, Add_Time = DateTime.Now.AddDays(-i) });
+            //}
+            //return View(new ContentViewModel { Contents = contents });
+
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DotNetFive/ViewModels/ContentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DotNetFive.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetFive.ViewModels
+{
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
